99751ea6fc8afa77e99c513a60bc3bd5c788c282
Shrihari-Nizamkar <shriharinzm@gmail.com>
2025-09-14 20:50:04 +0530

    3.component program

---
+++ 3.Components/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import Lists from "../components/Lists";
+
+function App() {
+  const name = "Shrihari";
+  return (
+    <>
+      <h1>Welcome React {name}</h1>
+      <Header />
+      <Lists />
+    </>
+  );
+}
+
+export default App;
