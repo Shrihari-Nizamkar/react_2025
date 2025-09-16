fc3b96cee045c8eb2f7d4cee62a05dbcb0facdc5
Shrihari-Nizamkar <shriharinzm@gmail.com>
2025-09-16 18:32:09 +0530

    digital clock functionality using useEffect

--- 6.Digital_Clock/src/App.jsx
+++ 6.Digital_Clock/src/App.jsx
@@ -1,31 +1,51 @@
 /*Task
 1. When you click on Start button then show current date and time
-    10:35:10 PM
 2. Make it live.
-
+3.Why is useEffect used here?
+🔹 Purpose: Cleanup on Component Unmount
+This useEffect runs only once when the component mounts ([] dependency array).
+It returns a cleanup function that will run when the component unmounts.
 */
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
-  const [time, setTime] = useState("");
+  const [time, setTime] = useState();
+  const intervalRef = useRef(null); // To store interval ID
 
   const updateTime = () => {
-    console.log("button clicked");
-    setTime(() => new Date().toLocaleTimeString());
-    setInterval(updateTime, 1000);
+    // Prevent multiple intervals
+    if (intervalRef.current !== null) return;
+
+    intervalRef.current = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
   };
 
-  //updateTime creates an infinite loop!
-  //Each time updateTime is called, it sets another interval, so it'll end up with many overlapping timers.
-
-  //so Clean up the interval when the component unmounts
-  //React needs a way to clear the interval if the component goes away — otherwise the timer keeps running in memory.
+  useEffect(() => {
+    //cleanup on unmount
+    //we return a cleanup function to clear the interval when the component unmounts.
+    return () => {
+      if (intervalRef.current !== null) {
+        clearInterval(intervalRef.current);
+      }
+    };
+  }, []);
 
   return (
     <>
       <h2>Time : {time}</h2>
       <button onClick={updateTime}>Start Live</button>
+      <button
+        onClick={() => {
+          if (intervalRef.current !== null) {
+            clearInterval(intervalRef.current);
+            intervalRef.current = null;
+          }
+        }}
+      >
+        Stop
+      </button>
     </>
   );
 }
