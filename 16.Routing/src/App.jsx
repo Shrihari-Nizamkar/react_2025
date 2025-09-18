60f1957bea34f216590dca1f7ae2f04656cb3a11
Shrihari-Nizamkar <shriharinzm@gmail.com>
2025-09-19 00:07:27 +0530

    16.react basic router

---
+++ 16.Routing/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
