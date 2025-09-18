5fb5e351aed3b2b213188256b02be3804a7776b2
Shrihari-Nizamkar <shriharinzm@gmail.com>
2025-09-19 00:14:17 +0530

    16.routing by menu and error page

--- 16.Routing/src/App.jsx
+++ 16.Routing/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 import Contact from "./Contact";
+import ErrorPage from "./ErrorPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </>
   );
--- 16.Routing/src/Home.jsx
+++ 16.Routing/src/Home.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <nav
+        style={{
+          display: "flex",
+          gap: "15px",
+          padding: "10px",
+          background: "#eee",
+        }}
+      >
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+      <h1>Home Page</h1>
+    </div>
+  );
 };
 
 export default Home;
