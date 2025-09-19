d6930c150392f71ab7c25cfbc4fc75ecf285a64a
Shrihari-Nizamkar <shriharinzm@gmail.com>
2025-09-19 16:54:31 +0530

    19.css and scss

---
+++ 19.CSS_SCSS/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Button from "./components/Button";
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <>
+      <Card />
+      <Button />
+    </>
+  );
+}
+
+export default App;
---
+++ 19.CSS_SCSS/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Card.module.scss";
+
+const Card = () => {
+  return (
+    <div className={styles.card}>
+      <h1 className={styles.title}>Rich Dad Poor Dad</h1>
+    </div>
+  );
+};
+
+export default Card;
