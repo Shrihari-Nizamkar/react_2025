1f4ab61c2acb621bcb8013a9b24d1f2ce3bc6132
Shrihari-Nizamkar <shriharinzm@gmail.com>
2025-09-26 22:43:05 +0530

    27.frontend

---
+++ 27.backend_frontend/frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Items from "./pages/Items";
+import ProtectedRoute from "./components/ProtectedRoutes";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Items />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+}
+
+export default App;
